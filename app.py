5772304d7b9b6f5e52c05cca26a9fbfcc8cc1e10
AngeloMakory <angelomakory@gmail.com>
2025-06-24 18:15:03 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="pdf_db"
+)
+cursor = db.cursor(dictionary=True)
+
+@app.route("/api/messages", methods=["GET"])
+def get_messages():
+    cursor.execute("SELECT * FROM messages")
+    messages = cursor.fetchall()
+    return jsonify(messages)
+
+@app.route("/api/messages", methods=["POST"])
+def add_message():
+    data = request.json
+    cursor.execute("INSERT INTO messages (content) VALUES (%s)", (data["content"],))
+    db.commit()
+    return jsonify({"message": "Added"}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
