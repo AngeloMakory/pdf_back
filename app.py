446ab0bdb656b960daeefec8b64e44553336cd84
EC2 Default User <ec2-user@ip-172-31-89-13.ec2.internal>
2025-07-14 20:13:13 +0000

    feat:fixed pymysqsql imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pymysql
-from pymysql import pooling
+#from pymysql import pooling
 import os
 from werkzeug.utils import secure_filename
 from PyPDF2 import PdfReader
